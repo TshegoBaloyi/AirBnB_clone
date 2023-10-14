c0d597ea161d88bebc3f87793cfed2f67b0f05e7
bongekile23 <nkosibl8@gmail.com>
2023-10-14 12:50:53 +0000

    AirBnB

--- pycodestyle.py
+++ pycodestyle.py
@@ -1,35 +1,8 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
+if __name__ == "__main__":
 
-    """Print x elememts of a list.
-
-    Args:
-
-        my_list (list): The list to print elements from.
-
-        x (int): The number of elements of my_list to print.
-
-    Returns:
-
-        The number of elements printed.
-
-    """
-
-    ret = 0
-
-    for i in range(x):
-
-        try:
-
-            print("{}".format(my_list[i]), end="")
-
-            ret += 1
-
-        except IndexError:
-
-            break
-
-    print("")
-
-    return (ret)
+    """Print the sum of 1 and 2."""
+    from add_0 import add                                      a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
