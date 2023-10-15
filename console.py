ab93afcca6c8a7d8c4e29b0084bfe2da97c7a226
Tshegofatso <tshegofatsosekhabi@gmail.com>
2023-10-15 10:11:40 -0700

    update

--- console.py
+++ console.py
@@ -17,7 +17,16 @@ from models.review import Review
 class HBNBCommand(cmd.Cmd):
     """defines command interpreter"""
     prompt = '(hbnb) '
+    instances = []
     classes = ['BaseModel']
+
+    def __init__(self):
+        """ init of the class"""
+        self.__class__.instances.append(self)
+
+        @classmethod
+        def all(cls):
+            return cls.instances
 
     def do_quit(self, arg):
         """Exit the program"""
@@ -40,68 +49,75 @@ class HBNBCommand(cmd.Cmd):
         print("Exit the program")
 
     def do_create(self, arg):
-        """Creates a new instance of User, saves it, and prints the id"""
-        if not args:
-            print("** class name missing **")
-            return
-        if args != "User":
-            print("** class doesn't exist **")
-            return
-        user = User()
-        user.save()
-        print(user.id)
-
-    def do_destroy(self, arg):
-        """Deletes a User instance based on the class name and id"""
-        if not args:
-            print("** class name missing **")
-            return
-        if args.split()[0] != "User":
-            print("** class doesn't exist **")
-            return
-        obj_id = args.split()[1]
-        if not obj_id:
-            print("** instance id missing **")
-            return
-        user = self.get_object_by_id(User, obj_id)
-        if not user:
-            print("** no instance found **")
-            return
-        user.delete()
-        self.save()
-
-    def do_show(self, arg):
-        """Prints the string representation of a User instance"""
-        if not args:
-            print("** class name missing **")
-            return
-        if args.split()[0] != "User":
-            print("** class doesn't exist **")
-            return
-        obj_id = args.split()[1]
-        if not obj_id:
-            print("** instance id missing **")
-            return
-        user = self.get_object_by_id(User, obj_id)
-        if not user:
-            print("** no instance found **")
-            return
-        print(user)
-
-    def do_all(self, arg):
-        """Prints all string representation of all the class name"""
+        """Create a new instance of a class"""
+        args = arg.split()
         if not arg:
             print("** class name missing **")
             return
 
-        try:
-            cls = eval(arg)
-        except NameError:
+        class_name = args[0]
+        classes = ["BaseModel", "city", "place", "state", "user"]
+
+        if class_name not in classes:
             print("** class doesn't exist **")
             return
 
-        instances = storage.all(cls)
-        print([str(instance) for instance in instances.values()])
+        new_instance = eval(class_name)()
+        new_instance.save()
+        print(new_instance.id)
+
+    def do_destroy(self, arg):
+        """Deletes a User instance based on the class name and id"""
+        classes = ["BaseModel", "user", "place", "city", "state"]
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name not in classes:
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        key = class_name + "." + obj_id
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        del storage.all()[key]
+        storage.save()
+
+    def do_show(self, arg):
+        """Prints the string representation of a User instance"""
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+        class_name = args[0]
+        if class_name != "User":
+            print("** class doesn't exist **")
+            return
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+        obj_id = args[1]
+        key = class_name + "." + obj_id
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+        print(storage.all()[key])
+
+    def do_all(self, arg):
+        """Print all instances"""
+        classes = ["BaseModel", "User", "Place", "Review", "City"]
+        if arg == "":
+            objects = self.__objects.values()
+        else:
+            class_name = arg.split()[0]
+            if class_name not in classes:
+                print("** class doesn't exist **")
+                return
 
     def do_update(self, arg):
         """Update an instance"""
@@ -134,10 +150,30 @@ class HBNBCommand(cmd.Cmd):
             return
 
         try:
-            instances = eval(args[0]).all()
+            class_name = args[0]
+            if class_name not in self.classes:
+                print("** class doesn't exist **")
+                return
+
+            instances = self.classes[class_name]
             print(len(instances))
         except NameError:
-            print("** class doesn't exist **")
+            i
+        print("** class doesn't exist **")
+
+    def do_help(self, arg):
+        if arg == "":
+            print("Documented commands (type help <topic>):")
+            print("========================================")
+            print("help           - List available commands")
+            print("quit           - Exit the program")
+        else:
+            if arg == "help":
+                print("check help manual")
+            elif arg == "quit":
+                print("type quit")
+            else:
+                print("No help available for '{}'".format(arg))
 
 
 if __name__ == '__main__':
