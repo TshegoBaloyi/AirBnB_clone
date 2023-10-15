45f4f5e87d06ee0251a8719dcd5cb58481d73fd9
Tshegofatso <tshegofatsosekhabi@gmail.com>
2023-10-15 01:32:47 -0700

    test files

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ Define unittest console.py """
 
+import os
+import sys
 import unittest
+from models.engine.file_storage import FileStorage
 from unittest.mock import patch
 from io import StringIO
 from console import HBNBCommand
